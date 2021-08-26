83eee34b156ddd8ce12d7853509f6ceb76f7a8e7
Rafael Silva <73067778+RafaeI11@users.noreply.github.com>
2021-08-26 16:32:30 -0300

    Fixed -name x2
    I'll NEVER code on mobile again

--- main.py
+++ main.py
@@ -147,12 +147,12 @@ async def on_message(message):
 
     # command to update a user's name
     if message.content.startswith("-name"):
-        if message.author.id not in msg_dic:
+        if str(message.author.id) not in msg_dic:
             pass
         else:
             name = str(message.author).split("#")
-            if name[0] == msg_dic[message.author.id]["name"]:
-                await message.channel.send("Your name is already updated")
+            if name[0] == msg_dic[str(message.author.id)]["name"]:
+                await message.channel.send("Your name is already up to date")
             else:
                 msg_dic[message.author.id]["name"] = name[0]
                 await message.channel.send(f"Name updated to {name[0]}")
