d025d11a23b23ea661c01d2e187357f510c4142c
Rafael Silva <73067778+RafaeI11@users.noreply.github.com>
2021-10-27 18:00:48 -0300

    Update file_saver.py

--- file_saver.py
+++ file_saver.py
@@ -1,20 +1,10 @@
 import json
-from time import sleep
 
-while True:
-    try:
-        with open("messages.json", "r") as a:
-            dic = json.loads(a.read())
-
+def saver():
+    with open("messages.json", "r") as a:
+        dic = json.loads(a.read())
         for key in dic:
             simple_dic = dic[key]
-
             with open(f"{key}.json", "w") as b:
                 json.dump(simple_dic, b, indent=4)
-
-        print("Backup done!")
-
-    except FileNotFoundError:
-        print("backup will be done on the next 24 hours if you run the bot")
-
-    sleep(86400)  # 24 hours
+    print("Backup done!")
